a91e6b96a40b6793aab57b89693101c3ef8bb110
Kristan Ruiz Limon <itan_07@hotmail.com>
2026-07-15 08:47:50 -0700

    styles: separating styles per page

--- src/pages/Dev.svelte
+++ src/pages/Dev.svelte
@@ -1,5 +1,5 @@
 <script>
-import Canvas from '../components/Main/Canvas.svelte';
+import Canvas from "../components/Widgets/Canvas.svelte";
 </script>
 
 <h1>Developer page (Testing and other stuff)</h1>
--- src/pages/Main.svelte
+++ src/pages/Main.svelte
@@ -46,30 +46,44 @@ const Layout_InitialContent: CustomRootContentItemsConfig<typeof Layout_InitialC
 };
 </script>
 
-<div class="layout-wrapper">
-  <GoldenLayout
-    bind:layout={Layout}
-    content={Layout_InitialContent}
-    components={Layout_InitialComponents}
-    defaultComponent={TestPanel}
-    // componentParts={{
-    //   header: 'bg-slate-900 border-b border-slate-700/50',
-    //   tab: 'bg-slate-800 text-slate-400 hover:bg-slate-700/80 transition-colors',
-    //   activeTab: 'bg-slate-750 text-blue-400 font-semibold border-t-2 border-blue-500',
-    //   content: 'bg-slate-950 text-slate-200 border border-slate-800',
-    //   splitter: 'bg-slate-900 hover:bg-blue-600/80 transition-colors duration-200',
-    // }}
-    // theme={{
-    //   layoutBg: '#0f172a',      // Slate-900
-    //   splitterBg: '#1e293b',    // Slate-800
-    //   splitterHoverBg: '#3b82f6', // Blue-500
-    // }}
-  />
-</div>
+<main id="root-app">
+  <div class="layout-wrapper">
+    <GoldenLayout
+      bind:layout={Layout}
+      content={Layout_InitialContent}
+      components={Layout_InitialComponents}
+      defaultComponent={TestPanel}
+      // componentParts={{
+      //   header: 'bg-slate-900 border-b border-slate-700/50',
+      //   tab: 'bg-slate-800 text-slate-400 hover:bg-slate-700/80 transition-colors',
+      //   activeTab: 'bg-slate-750 text-blue-400 font-semibold border-t-2 border-blue-500',
+      //   content: 'bg-slate-950 text-slate-200 border border-slate-800',
+      //   splitter: 'bg-slate-900 hover:bg-blue-600/80 transition-colors duration-200',
+      // }}
+      // theme={{
+      //   layoutBg: '#0f172a',      // Slate-900
+      //   splitterBg: '#1e293b',    // Slate-800
+      //   splitterHoverBg: '#3b82f6', // Blue-500
+      // }}
+    />
+  </div>
+</main>
 
 <style>
+  #root-app {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
   .layout-wrapper {
-    width: 95vw;
-    height: 95vh;
+    width: 95%;
+    height: 95%;
+  }
+
+  :global(body) {
+    overflow: hidden;
   }
 </style>
